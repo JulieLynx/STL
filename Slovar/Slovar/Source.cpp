56c9cc64bdd3facf9248e8443a74f01a830e9860
Student <Student@201-31>
2019-09-30 19:05:35 +0300

    1c

---
+++ Slovar/Slovar/Source.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+#include<map>
+#include<vector>
+
+using namespace std;
+using std::cout;
+using std::cin;
+
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	map<string, vector<string>>  dictionary =
+	{
+		pair<string,vector<string>>("pain",vector<string>{"боль","огорчение"}),
+		pair<string,vector<string>>("love",vector<string>{"влюбленность","любовь"}),
+		pair<string,vector<string>>("space",vector<string>{"космос","пространство"}),
+		pair<string,vector<string>>("void",vector<string>{"пустота","вакууум"}),
+		pair<string,vector<string>>("solitude",vector<string>{"одиночество","уединение"}),
+		pair<string,vector<string>>("winter",vector<string>{"холод","зима"}),
+	};
+
+
+
+
+}
