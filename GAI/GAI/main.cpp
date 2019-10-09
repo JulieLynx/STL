5474056fc5a21ae68385ecad9785cbb55048619a
Student <Student@201-31>
2019-10-09 20:02:12 +0300

    11c

--- GAI/GAI/main.cpp
+++ GAI/GAI/main.cpp
@@ -10,37 +10,56 @@ using std::cin;
 
 void print(const map < string, forward_list<Crime>>& database);
 void print(const map < string, forward_list<Crime>>& database, const string& id);
+void print(const map < string, forward_list<Crime>>& database, const string& start_id, const string& end_id);
 
 void main()
 {
 	setlocale(LC_ALL, "");
 	map<string, forward_list<Crime>>database =
 	{
-		pair<string,forward_list<Crime>>("BI6666BC",{Crime("АТБ на Ляли","Принес в жертву черного барана, в результате чего облил светор кровью барана,и заляпал зебру")}),
-		pair<string,forward_list<Crime>>("BI4567BC", {Crime("McDonalds", "Плюнул в пост Гаи")}),
-		pair<string,forward_list<Crime>>("BT2134CI", {Crime("АТБ на Ляли", "Сбил забор возле клумбы")})
+		pair<string,forward_list<Crime>>("BI 6666 BC",{Crime("АТБ на Ляли","Принес в жертву черного барана, в результате чего облил светор кровью барана,и заляпал зебру")}),
+		pair<string,forward_list<Crime>>("BI 4567 BC", {Crime("McDonalds", "Плюнул в пост Гаи")}),
+		pair<string,forward_list<Crime>>("BT 2134 CI", {Crime("АТБ на Ляли", "Сбил забор возле клумбы")})
 
 	};
 	do
 	{
-	char key;
-	cout << "\n1. Вывод всей базы;" << endl;
-	cout << "2. Вывод информации по номеру;" << endl;
-	cout << "3. Вывод информации по диапазону номеров;" << endl;
-	cout << "4. Добавление записи;" << endl;
-	cout << "Выберите действие: "; cin >> key;
+		system("CLS");
+		char key;
+		cout << "\n1. Вывод всей базы;" << endl;
+		cout << "2. Вывод информации по номеру;" << endl;
+		cout << "3. Вывод информации по диапазону номеров;" << endl;
+		cout << "4. Добавление записи;" << endl;
+		cout << "Выберите действие: "; cin >> key;
 		switch (key)
 		{
-		case'1':print(database); break;
+		case'1': system("CLS"); print(database); break;
 		case'2':
 		{
+			system("CLS");
 			string id;
-			cout << "Введите номер автомобиля: "; cin >> id;
+			cout << "Введите номер автомобиля: "; cin.clear();
+			cin.clear();
+			cin.ignore();
+			getline(cin, id);
 			print(database, id);
+			break;
 		}
-		break;
+		case'3':system("CLS");
+		{
+			string start_id, end_id;
+			cout << "Введите начальный номер:  "; 
+			cin.clear();
+			cin.ignore();
+			getline(cin, start_id);
+			cout << "Введите конечный номер:  "; 
+			//cin.clear();
+			cin.ignore();
+			getline(cin, end_id);
+			print(database, start_id, end_id);
 		}
-	} while (getch()!=27);//для гетч нужно подключить conio.h !
+		}
+	} while (getch() != 27);//для гетч нужно подключить conio.h !
 	//	cout<< database.size() << endl;
 }
 void print(const map< string, forward_list<Crime >> &database)
@@ -67,6 +86,23 @@ void print(const map< string, forward_list<Crime >> &database)
 				cout << j.get_place() << "\t\n" << j.get_crime() << endl;
 			}
 			break;
+		}
+	}
+	if (!present_in_base)cout << "В базе нет такого номера" << endl;
+}
+void print(const map < string, forward_list<Crime>>& database,  const string& start_id, const string& end_id)
+{
+	//cout<<database[id].
+	bool present_in_base = false;
+	for (pair<string, forward_list<Crime>> i : database)
+	{
+		if (i.first >= start_id && i.first <= end_id)
+		{
+			present_in_base = true;
+			for (Crime j : i.second)
+			{
+				cout << j.get_place() << "\t\n" << j.get_crime() << endl;
+			}
 		}
 	}
 	if (!present_in_base)cout << "В базе нет такого номера" << endl;
